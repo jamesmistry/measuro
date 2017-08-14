554eafac09db03e0141afc7c0ea7f0b01a54ecbc
James Mistry <james.mistry@gmail.com>
2017-08-14 23:56:54 +1200

    Replaced templating with member function overloads and added registry lookup

--- src/Measuro.hpp
+++ src/Measuro.hpp
@@ -6,9 +6,33 @@
 #include <chrono>
 #include <atomic>
 #include <functional>
+#include <map>
+#include <vector>
+#include <utility>
+#include <memory>
+#include <exception>
+#include <ostream>
+#include <mutex>
 
 namespace measuro
 {
+
+    class MeasuroError : public std::runtime_error
+    {
+    public:
+        MeasuroError(std::string description) : std::runtime_error(description.c_str())
+        {
+        }
+    };
+
+    class MetricNameError : public MeasuroError
+    {
+    public:
+        MetricNameError(std::string description) : MeasuroError(description)
+        {
+        }
+    };
+
     /*!
     *
     *
@@ -16,7 +40,7 @@ namespace measuro
     * @param minor
     * @param release
     */
-    static void version(unsigned int & major, unsigned int & minor, unsigned int & release) noexcept
+    inline static void version(unsigned int & major, unsigned int & minor, unsigned int & release) noexcept
     {
         major = @LIB_VERSION_MAJOR@;
         minor = @LIB_VERSION_MINOR@;
@@ -28,17 +52,26 @@ namespace measuro
     *
     * @param version_str
     */
-    static void version_text(std::string & version_str) noexcept(false)
+    inline static void version_text(std::string & version_str) noexcept(false)
     {
         version_str = "@LIB_VERSION_MAJOR@.@LIB_VERSION_MINOR@-@LIB_VERSION_REL@";
     }
 
-    /*!
-     *
-     */
-    enum class MetricKind { UNSIGNED_INT, SIGNED_INT, FLOAT, RATE, STRING, ROLLING_AVG, BOOL, SUM };
+    inline static void copyright_text(std::string & copyright_str) noexcept(false)
+    {
+        copyright_str = "Measuro version @LIB_VERSION_MAJOR@.@LIB_VERSION_MINOR@-@LIB_VERSION_REL@\n\nCopyright (c) 2017, James Mistry. Released under the MIT licence - for details see https://github.com/jamesmistry/measuro";
+    }
 
     using DeadlineUnit = std::chrono::milliseconds;
+
+    enum class UINT { KIND };
+    enum class INT { KIND };
+    enum class FLOAT { KIND };
+    enum class RATE { KIND };
+    enum class STR { KIND };
+    enum class MOV_AVG { KIND };
+    enum class BOOL { KIND };
+    enum class SUM { KIND };
 
     /*!
      * @class Metric
@@ -48,10 +81,12 @@ namespace measuro
     class Metric
     {
     public:
-        enum class Kind { UNSIGNED_INT, SIGNED_INT, FLOAT, RATE, STRING, MOVING_AVG, BOOL, SUM };
+        enum class Kind { UINT = 0, INT = 1, FLOAT = 2, RATE = 3, STR = 4, MOV_AVG = 5, BOOL = 6, SUM = 7 };
 
-        Metric(Kind kind, const char * name, const char * unit = nullptr, DeadlineUnit rate_limit_1_per = DeadlineUnit::zero()) noexcept
-        : m_kind(kind), m_name(name), m_unit(((unit == nullptr) ? "" : unit)), m_last_updated(std::chrono::steady_clock::now()), m_rate_limit(rate_limit_1_per)
+        Metric(Kind kind, std::string name, std::string unit, std::string description,
+                std::function<std::chrono::steady_clock::time_point ()> time_function, DeadlineUnit rate_limit_1_per = DeadlineUnit::zero()) noexcept
+        : m_kind(kind), m_name(name), m_unit(unit), m_description(description),
+          m_last_updated(time_function()), m_time_function(time_function), m_rate_limit(rate_limit_1_per)
         {
         }
 
@@ -59,14 +94,19 @@ namespace measuro
         {
         }
 
-        const std::string & name() const noexcept
+        std::string name() const noexcept(false)
         {
             return m_name;
         }
 
-        const std::string & unit() const noexcept
+        std::string unit() const noexcept(false)
         {
             return m_unit;
+        }
+
+        std::string description() const noexcept(false)
+        {
+            return m_description;
         }
 
         Kind kind() const noexcept
@@ -74,71 +114,66 @@ namespace measuro
             return m_kind;
         }
 
-        void kind_name(std::string & name) const noexcept
+        std::string kind_name() const noexcept(false)
         {
             switch(m_kind)
             {
-            case Kind::UNSIGNED_INT:
-                name = "UNSIGNED_INT";
-                break;
-            case Kind::SIGNED_INT:
-                name = "SIGNED_INT";
-                break;
+            case Kind::UINT:
+                return "UINT";
+            case Kind::INT:
+                return "INT";
             case Kind::FLOAT:
-                name = "FLOAT";
-                break;
+                return "FLOAT";
             case Kind::RATE:
-                name = "RATE";
-                break;
-            case Kind::STRING:
-                name = "STRING";
-                break;
-            case Kind::MOVING_AVG:
-                name = "MOVING_AVG";
-                break;
+                return "RATE";
+            case Kind::STR:
+                return "STR";
+            case Kind::MOV_AVG:
+                return "AVG";
             case Kind::BOOL:
-                name = "BOOL";
-                break;
+                return "BOOL";
             case Kind::SUM:
-                name = "SUM";
-                break;
+                return "SUM";
             }
         }
 
-        virtual operator std::string() = 0;
+        virtual operator std::string() const = 0;
 
     protected:
         void update(std::function<void()> update_logic) noexcept(false)
         {
             if ((m_rate_limit == DeadlineUnit::zero()) ||
-                    (std::chrono::steady_clock::now() - m_last_updated) >= m_rate_limit)
+                    (m_time_function() - m_last_updated) >= m_rate_limit)
             {
                 update_logic();
-                m_last_updated = std::chrono::steady_clock::now();
+                m_last_updated = m_time_function();
             }
         }
 
     private:
-        std::string m_name;
         Kind m_kind;
+        std::string m_name;
         std::string m_unit;
+        std::string m_description;
         std::chrono::steady_clock::time_point m_last_updated;
+        std::function<std::chrono::steady_clock::time_point ()> m_time_function;
         DeadlineUnit m_rate_limit;
 
     };
 
-    class UnsignedMetric : public Metric
+    class UintMetric : public Metric
     {
     public:
-        UnsignedMetric(const char * name, const char * unit = nullptr, std::uint64_t initial_value = 0, DeadlineUnit rate_limit_1_per = DeadlineUnit::zero()) noexcept
-        : m_value(initial_value), Metric(Metric::Kind::UNSIGNED_INT, name, unit, rate_limit_1_per)
+        UintMetric(std::string name, std::string unit, std::string description, std::function<std::chrono::steady_clock::time_point ()> time_function,
+                std::uint64_t initial_value = 0, DeadlineUnit rate_limit_1_per = DeadlineUnit::zero()) noexcept
+        : Metric(Metric::Kind::UINT, name, unit, description, time_function, rate_limit_1_per), m_value(initial_value)
         {
         }
 
-        UnsignedMetric(const UnsignedMetric &) = delete;
-        UnsignedMetric(UnsignedMetric &&) = delete;
-        UnsignedMetric & operator=(const UnsignedMetric &) = delete;
-        UnsignedMetric & operator=(UnsignedMetric &&) = delete;
+        UintMetric(const UintMetric &) = delete;
+        UintMetric(UintMetric &&) = delete;
+        UintMetric & operator=(const UintMetric &) = delete;
+        UintMetric & operator=(UintMetric &&) = delete;
 
         operator std::string() const noexcept(false)
         {
@@ -160,70 +195,235 @@ namespace measuro
 
         std::uint64_t operator++() noexcept
         {
-            update([this]()
+            std::uint64_t new_val = this->m_value;
+
+            update([this, & new_val]()
             {
-                this->m_value++;
+                new_val = ++this->m_value;
             });
 
-            return m_value;
+            return new_val;
         }
 
         std::uint64_t operator++(int) noexcept
         {
-            std::uint64_t old = m_value;
+            std::uint64_t old_val = this->m_value;
 
-            update([this]()
+            update([this, & old_val]()
             {
-                this->m_value++;
+                old_val = this->m_value++;
             });
 
-            return old;
+            return old_val;
         }
 
         std::uint64_t operator--() noexcept
         {
-            update([this]()
+            std::uint64_t new_val = this->m_value;
+
+            update([this, & new_val]()
             {
-                this->m_value--;
+                new_val = --this->m_value;
             });
 
-            return m_value;
+            return new_val;
         }
 
         std::uint64_t operator--(int) noexcept
         {
-            std::uint64_t old = m_value;
+            std::uint64_t old_val = this->m_value;
 
-            update([this]()
+            update([this, & old_val]()
             {
-                this->m_value--;
+                old_val = this->m_value--;
             });
 
-            return old;
+            return old_val;
         }
 
         std::uint64_t operator+=(const std::uint64_t & rhs) noexcept
         {
-            update([this, rhs]()
+            std::uint64_t new_val = this->m_value;
+
+            update([this, rhs, & new_val]()
             {
-                this->m_value += rhs;
+                new_val = this->m_value += rhs;
             });
 
-            return m_value;
+            return new_val;
         }
 
         std::uint64_t operator-=(const std::uint64_t & rhs) noexcept
         {
-            update([this, rhs]()
+            std::uint64_t new_val = this->m_value;
+
+            update([this, rhs, & new_val]()
             {
-                this->m_value -= rhs;
+                new_val = this->m_value -= rhs;
             });
 
-            return m_value;
+            return new_val;
         }
 
     private:
         std::atomic<std::uint64_t> m_value;
+
+    };
+
+    class Renderer
+    {
+    public:
+        Renderer()
+        {
+        }
+
+        virtual ~Renderer()
+        {
+        }
+
+        virtual void before()
+        {
+            return;
+        }
+
+        virtual void after()
+        {
+            return;
+        }
+
+        virtual void render(const std::shared_ptr<Metric> & metric)
+        {
+            (void)(metric);
+            return;
+        }
+
+    };
+
+    class TextRenderer : Renderer
+    {
+    public:
+        TextRenderer(std::ostream & destination) : m_destination(destination)
+        {
+        }
+
+        virtual ~TextRenderer()
+        {
+        }
+
+        void after()
+        {
+            m_destination << std::endl;
+        }
+
+        void render(const std::shared_ptr<Metric> & metric)
+        {
+            m_destination << metric->name() << " = " << metric << "\n";
+        }
+
+    private:
+        std::ostream & m_destination;
+
+    };
+
+    class Registry
+    {
+    public:
+        Registry(std::function<std::chrono::steady_clock::time_point ()> time_function = []{return std::chrono::steady_clock::now();}) noexcept
+        : m_time_function(time_function)
+        {
+        }
+
+        std::shared_ptr<UintMetric> create_metric(UINT, std::string name, std::string unit, std::string description,
+                std::uint64_t initial_value = 0, DeadlineUnit rate_limit_1_per = DeadlineUnit::zero()) noexcept(false)
+        {
+            // TODO: Replace with std::scoped_lock on migration to C++17
+            std::lock_guard<std::mutex> lock(m_registry_mutex);
+
+            auto metric = std::shared_ptr<UintMetric>(new UintMetric(name, unit, description, m_time_function, initial_value, rate_limit_1_per));
+            register_metric<UintMetric>(name, metric, m_uint_metrics);
+            return metric;
+        }
+
+        std::shared_ptr<UintMetric> operator()(UINT, std::string name) noexcept(false)
+        {
+            // TODO: Replace with std::scoped_lock on migration to C++17
+            std::lock_guard<std::mutex> lock(m_registry_mutex);
+
+            auto entry = lookup(name);
+            return m_uint_metrics[entry->second.second];
+        }
+
+        void render(Renderer & renderer, std::string name_prefix = "") noexcept(false)
+        {
+            // TODO: Replace with std::scoped_lock on migration to C++17
+            std::lock_guard<std::mutex> lock(m_registry_mutex);
+
+            RendererContext render_ctx(renderer);
+
+            for (auto metric : m_metrics)
+            {
+                if ((name_prefix.length() == 0) || (metric.first.find(name_prefix) == 0))
+                {
+                    renderer.render(metric.second.first);
+                }
+            }
+        }
+
+    private:
+        class RendererContext
+        {
+        public:
+            RendererContext(Renderer & renderer) : m_renderer(renderer)
+            {
+                m_renderer.before();
+            }
+
+            ~RendererContext()
+            {
+                m_renderer.after();
+            }
+
+        private:
+            Renderer & m_renderer;
+        };
+
+        std::map<std::string, std::pair<std::shared_ptr<Metric>, std::uint64_t> >::const_iterator
+        lookup(const std::string & name) const noexcept(false)
+        {
+            auto entry = m_metrics.find(name);
+            if (entry == m_metrics.end())
+            {
+                throw MetricNameError("No metric exists with the name \"" + name + "\"");
+            }
+
+            return entry;
+        }
+
+        template<typename T>
+        void register_metric(std::string & metric_name, std::shared_ptr<T> & metric, std::vector<std::shared_ptr<T> > & metric_registry) noexcept(false)
+        {
+            if (metric_name.length() == 0)
+            {
+                // Anonymous metric - not associated with a name
+                metric_registry.push_back(metric);
+            }
+            else
+            {
+                if (m_metrics.find(metric_name) != m_metrics.end())
+                {
+                    throw MetricNameError("A metric already exists with the name \"" + metric_name + "\"");
+                }
+
+                metric_registry.push_back(metric);
+                m_metrics[metric_name] = std::pair<std::shared_ptr<Metric>, std::uint64_t>(metric, metric_registry.size() - 1);
+            }
+        }
+
+        std::mutex m_registry_mutex;
+        std::function<std::chrono::steady_clock::time_point ()> m_time_function;
+        std::map<std::string, std::pair<std::shared_ptr<Metric>, std::uint64_t> > m_metrics;
+
+        std::vector<std::shared_ptr<UintMetric> > m_uint_metrics;
 
     };
 }
