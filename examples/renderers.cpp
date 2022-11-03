d71a1c38ef2af28de1f27810e700195c22bbee37
jamesmistry <james.mistry@gmail.com>
2022-11-03 17:22:11 +0000

    Updated renderer example to include Prometheus renderer

--- examples/renderers.cpp
+++ examples/renderers.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <chrono>
 
 #include "measuro.hpp"
 
@@ -7,7 +8,9 @@ int main(int argc, char * argv[])
 {
     measuro::Registry reg;
 
-    auto count_metric = reg.create_metric(measuro::INT::KIND, "example_count",
+    auto count_metric_1 = reg.create_metric(measuro::INT::KIND, "example_count",
+            "item(s)", "An example count metric", 0);
+    auto count_metric_2 = reg.create_metric(measuro::INT::KIND, "example_count2",
             "item(s)", "An example count metric", 0);
     auto str_metric = reg.create_metric(measuro::STR::KIND, "example_str",
             "An example string metric");
@@ -16,17 +19,26 @@ int main(int argc, char * argv[])
 
     for (auto i=0;i<100;++i)
     {
-        ++*count_metric;
+        ++*count_metric_1;
     }
+
+    *count_metric_2 = 1234;
 
     measuro::PlainRenderer pl_renderer(std::cout);
     measuro::JsonRenderer js_renderer(std::cout);
+    measuro::PrometheusRenderer pr_renderer(std::cout, []()
+            { 
+                /* Assumes C++20 */ 
+                return std::chrono::duration_cast<std::chrono::milliseconds>(
+                        std::chrono::system_clock::now().time_since_epoch()).count(); 
+            }, "example_app");
 
     std::cout << "Plain text output:\n\n";
     reg.render(pl_renderer);
     std::cout << "----------\n\nJSON output:\n\n";
     reg.render(js_renderer);
 
-    std::cout << std::endl; // The JSON renderer doesn't terminate with a newline!
+    std::cout << "\n\n"; // The JSON renderer doesn't terminate with a newline!
 
+    reg.render(pr_renderer); // Prometheus renderer always ends with a newline!
 }
